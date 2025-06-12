401e9aa54684935efe96c746e47cf9946b9c6540
Someone12543 <47923692+Someone12543@users.noreply.github.com>
2025-06-12 16:10:41 -0300

    Update testes.py
    Linhas duplicadas removidas

--- api/svg/testes.py
+++ api/svg/testes.py
@@ -14,10 +14,14 @@ class Svg_converter:
         image_np = np.array(image)
 
         # Apply a threshold
-        _, binary = cv.threshold(image_np, 130, 255, cv.THRESH_TOZERO_INV)
-
+        _, binary = cv.threshold(image_np, 150, 255, cv.THRESH_TOZERO_INV)
+        
+        # Skeletonize (single-pixel-wide lines)
+        skeleton = skeletonize(binary)
+        skeleton = (skeleton * 255).astype(np.uint8)
+        
         # Find contours with this threshold
-        contours, _ = cv.findContours(binary, cv.RETR_TREE , cv.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv.findContours(skeleton, cv.RETR_LIST , cv.CHAIN_APPROX_SIMPLE)
 
         # Create new SVG with contours
         dwg = svgwrite.Drawing(size=(image.width, image.height))
