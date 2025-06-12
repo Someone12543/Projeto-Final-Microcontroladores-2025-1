6967e615690588a0156ee548f7e1460730ca0c46
Someone12543 <pedro_gm03@hotmail.com>
2025-06-12 09:00:14 -0300

    svg converter
    teste incluido

---
+++ api/svg/testes.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy as np
+import cv2 as cv
+import svgwrite
+
+class Svg_converter:
+    def __init__(self):
+        pass
+    def convert(self, image_path, svg_path):
+        # Load theimage
+        image = Image.open(image_path).convert("L")  # Convert to grayscale
+
+        # Convert image to numpy array
+        image_np = np.array(image)
+
+        # Apply a threshold
+        _, binary = cv.threshold(image_np, 130, 255, cv.THRESH_TOZERO_INV)
+
+        # Find contours with this threshold
+        contours, _ = cv.findContours(binary, cv.RETR_TREE , cv.CHAIN_APPROX_SIMPLE)
+
+        # Create new SVG with contours
+        dwg = svgwrite.Drawing(size=(image.width, image.height))
+        for contour in contours:
+            points = [(int(p[0][0]), int(p[0][1])) for p in contour]
+            dwg.add(dwg.polyline(points=points, stroke='black', fill='none', stroke_width=1))
+
+        # Save the SVG
+        dwg.saveas(svg_path)
+
+Converter = Svg_converter()
+
+Converter.convert("maca.png", "maca.svg")
