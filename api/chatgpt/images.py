53a2513889859923b72bfa5d178b83a7e3b09eb5
Someone12543 <pedro_gm03@hotmail.com>
2025-06-26 11:33:25 -0300

    progresso

---
+++ api/chatgpt/images.py
@@ -0,0 +1,45 @@
+import openai
+from dotenv import load_dotenv
+import base64
+import os
+
+class ImageGenerator():
+    def __init__(self):
+        pass
+
+    def generate_image(self, user_prompt, quality, colors):
+        load_dotenv()
+
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+        client = openai.OpenAI() 
+
+        prompt = "Generate me an image, only drawing lines with no solid elements, \
+following the given structure, where 'Subject' is the main idea being drawn; \
+'Detail' includes LOW, MEDIUM, and HIGH, where LOW is the minimum amount of \
+detail to have it be identified as the subject, MEDIUM is a reasonable amount \
+of detail, but not much, and HIGH is a little more detail than MEDIUM, making \
+sure it's not too realistic; 'Colors' is the selection of colors to generate \
+the image with, having the lines be colored where you believe the colors should go. \
+Make sure to not fill in any elements when coloring, limiting the image to only lines\n\
+Subject: " + user_prompt + "\n\
+Detail: " + quality + "\n\
+Colors: " + colors + "\n"
+
+        print(prompt)
+
+        generation_response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            n=1,
+            size="1024x1024",
+            response_format="url",
+        ) 
+        print(response.data[0].url)
+
+
+PROMPT = "A vaporwave computer"
+
+generator = ImageGenerator()
+
+generator.generate_image(PROMPT, "HIGH", "black, blue")
--- api/svg/testes.py
+++ api/svg/testes.py
@@ -1,7 +1,9 @@
 from PIL import Image
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 import svgwrite
+from skimage.morphology import skeletonize
 
 class Svg_converter:
     def __init__(self):
@@ -23,12 +25,20 @@ class Svg_converter:
         # Find contours with this threshold
         contours, _ = cv.findContours(skeleton, cv.RETR_LIST , cv.CHAIN_APPROX_SIMPLE)
 
+        ax = plt.subplot(1, 1, 1)
+        
         # Create new SVG with contours
         dwg = svgwrite.Drawing(size=(image.width, image.height))
         for contour in contours:
             points = [(int(p[0][0]), int(p[0][1])) for p in contour]
             dwg.add(dwg.polyline(points=points, stroke='black', fill='none', stroke_width=1))
 
+            for point in points:
+                ax.plot(point[0], point[1], 'o')
+                plt.show()
+
+            
+        
         # Save the SVG
         dwg.saveas(svg_path)
 
