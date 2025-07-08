7ed0949610984cc452826e445483ef4091bbf0a8
Joao Biscaia <joaopedro.jpbf@gmail.com>
2025-07-08 14:19:10 -0300

    fix dimensoes imagem

--- api/svg/communicator.py
+++ api/svg/communicator.py
@@ -1,17 +1,22 @@
 from svgelements import SVG
 from serial import Serial
 from time import sleep
+import xml.etree.ElementTree as ET
 
 class Svg_communicator:
     def __init__(self):
         pass
     
     def communicate(self, filename):
-        meu_serial = Serial("COM23", baudrate=115200)
-        sleep(2)
-        
+        try:
+            meu_serial = Serial("/dev/ttyACM0", baudrate=115200)
+        except Exception as e:
+            raise e
+
         count = 1
-        
+        infos = ET.parse(filename)
+        width = infos.attrib.get('width')
+        height = infos.attrib.get('height')
         
         svg = SVG.parse(filename)
         for element in svg:
@@ -23,7 +28,10 @@ class Svg_communicator:
             sleep(0.05)
                               
             for point in element.points:
-                ponto = str(int(point.x) * 2) + ',' + str(int(point.y) * 2) + '\n'
+                if (width < 600) or (height < 600):
+                    ponto = str(int(point.x) * 2) + ',' + str(int(point.y) * 2) + '\n'
+                else:
+                    ponto = str(int(point.x) ) + ',' + str(int(point.y)) + '\n'
                 meu_serial.write( (ponto).encode("UTF-8") )
                 
                 while 1:
@@ -38,4 +46,4 @@ class Svg_communicator:
 
 if __name__ == "__main__":
     communicator = Svg_communicator()
-    communicator.communicate("maca.svg")
+    communicator.communicate("../output/image.svg")
