798f403b0e8a61d5fed8f48060f72955f30502fd
joaocapechi <126024484+joaocapechi@users.noreply.github.com>
2025-07-10 23:05:38 -0300

    Update communicator.py

--- api/svg/communicator.py
+++ api/svg/communicator.py
@@ -9,11 +9,16 @@ class Svg_communicator:
     
     def communicate(self, filename):
         try:
-            meu_serial = Serial("/dev/ttyACM0", baudrate=115200)
+            meu_serial = Serial("COM30", baudrate=115200)
+            sleep(2)
+            # meu_serial = Serial("/dev/ttyACM0", baudrate=115200)
         except Exception as e:
+            print(e)
             raise e
+        
 
         count = 1
+        print(filename)
         infos = ET.parse(filename).getroot()
         width = infos.get('width')
         height = infos.get('height')
@@ -21,17 +26,25 @@ class Svg_communicator:
         
         svg = SVG.parse(filename)
         for element in svg:
-            meu_serial.write(("0\n").encode("UTF-8"))
+            try:
+                #print(type(element.stroke.value))
+                color = (element.stroke.value)
+                if color == 255:
+                    color = "0\n"
+                elif color == 122:
+                    color = "1\n"
+                else:
+                    color = "2\n"
+            except:
+                color = "0\n"
+            
+            meu_serial.write( (color).encode("UTF-8"))
             sleep(0.05)
             
             qtd = str(len(element.points)) + "\n"            
             meu_serial.write( (qtd).encode("UTF-8"))
             sleep(0.05)
-
-            color = str(len(element.stroke)) + "\n"            
-            meu_serial.write( (color).encode("UTF-8"))
-            sleep(0.05)
-                              
+                           
             for point in element.points:
                 if (int(width) < 600) or (int(height) < 600):
                     ponto = str(int(point.x) * 2) + ',' + str(int(point.y) * 2) + '\n'
@@ -48,7 +61,7 @@ class Svg_communicator:
             count += 1
                              
 
-
 if __name__ == "__main__":
     communicator = Svg_communicator()
-    communicator.communicate("../output/image.svg")
+    communicator.communicate("circle.svg")
+    #communicator.communicate("../output/image.svg")
